971ec2a306f0523fd27393f9358769983345129e
“Mukti” <muktijindal5@gmail.com>
2026-02-07 13:00:11 +0530

    changes in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -84,7 +84,7 @@ const Login = () => {
             <h2 className="text-xl font-bold text-[#0078db] mb-2">
               Rozgar Dwar
             </h2>
-            <h3 className="text-2xl font-semibold mb-6">Login</h3>
+            <h3 className="text-2xl font-semibold mb-6">Login as Consultant</h3>
 
             <form onSubmit={handleSubmit} className="space-y-4">
               {/* Username */}
